c677ed19a7eae0d935508bc213b8d1a261b88e7b
Arjun Kumar <mrarjunkumar42@gmail.com>
2024-12-12 10:51:33 -0600

    folder removed

--- digital-market/src/main/java/com/digitalmarket/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.digitalmarket.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-
-	public ProductNotFoundException(String message) {
-		super(message);
-	}
-
-}
--- digital-market/src/main/java/com/digitalmarket/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.digitalmarket.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-import java.util.*;
-import com.digitalmarket.model.*;
-public interface OrderRepository extends JpaRepository<OrderEntity,Integer> {
-OrderEntity getByUserIdUserId(Integer userId);
-}
